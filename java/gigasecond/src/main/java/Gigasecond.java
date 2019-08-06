9078dae8e31164e955e1f19a787793d6fba4feee
Serafeim Papastefanos <spapas@gmail.com>
2019-08-06 17:28:46 +0300

    imrpove java gigasecond

--- java/gigasecond/src/main/java/Gigasecond.java
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -1,17 +1,16 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 class Gigasecond {
     public final long GIGASECOND = 1000000000;
     LocalDateTime time;
 
     Gigasecond(LocalDate moment) {
-        time = moment.atStartOfDay().plus(GIGASECOND, ChronoUnit.SECONDS);
+        this(moment.atStartOfDay());
     }
 
     Gigasecond(LocalDateTime moment) {
-        time = moment.plus(GIGASECOND, ChronoUnit.SECONDS);
+        time = moment.plusSeconds(GIGASECOND);
     }
 
     LocalDateTime getDateTime() {
