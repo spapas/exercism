8b36d84d12cd9465e078abdb7c086c6f6c6f5b98
Serafeim Papastefanos <spapas@gmail.com>
2019-08-06 14:06:33 +0300

    Add resitor color duo js

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const value = ([v1, v2]) => {
+  return COLORS.indexOf(v1)*10 + COLORS.indexOf(v2)
+};
+
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+
