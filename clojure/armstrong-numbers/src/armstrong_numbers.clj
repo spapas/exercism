2d09c455e2b3b396bdf11fe262b68f678471cbd6
Serafeim Papastefanos <spapas@gmail.com>
2019-08-06 13:35:21 +0300

    improve clojure armstrong

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,11 +1,8 @@
 (ns armstrong-numbers)
+
 (defn armstrong? [num]
-  (let [
-    digits (str num) 
+  (let [digits (str num) 
     exp (count (str num))
     sum #(reduce + %1)
-    power_mapper #(-> %1 (Character/digit 10) biginteger (.pow exp))
-  ]
-  (= (sum (map power_mapper digits)) num)
-  )
-)
+    power-mapper #(-> %1 (Character/digit 10) biginteger (.pow exp))]
+  (= (sum (map power-mapper digits)) num)))
