4a7f1554d4da57c2e140c6532951c81f877ac198
Serafeim Papastefanos <spapas@gmail.com>
2019-08-07 18:13:45 +0300

    Improve js gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,9 +1,5 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const GIGASECOND_IN_MS = 1e9 * 1000;
 
 export const gigasecond = (time) => {
-  const GIGASECOND_IN_MS = 1e9 * 1000;
   return new Date(time.getTime() + GIGASECOND_IN_MS)
 };
