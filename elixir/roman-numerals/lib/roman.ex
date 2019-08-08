caacbd74448f6678a871c09932ffe64fcae065cc
Serafeim Papastefanos <spapas@gmail.com>
2019-08-08 23:43:01 +0300

    do roman/elixir in different style

--- elixir/roman-numerals/lib/roman.ex
+++ elixir/roman-numerals/lib/roman.ex
@@ -1,45 +1,52 @@
 defmodule Roman do
-  @symbols %{
-    1 => "I",
-    5 => "V",
-    10 => "X",
-    50 => "L",
-    100 => "C",
-    500 => "D",
-    1000 => "M"
-  }
+  @symbols [
+    {1000, "M"},
+    {900, "CM"},
+    {500, "D"},
+    {400, "CD"},
+    {100, "C"},
+    {90, "XC"},
+    {50, "L"},
+    {40, "XL"},
+    {10, "X"},
+    {9, "IX"},
+    {5, "V"},
+    {4, "IV"},
+    {1, "I"}
+  ]
+
+  # The reducer checks to see if the current number is >= than the
+  # number in the accumulator and if yes it will
+  # * Update the result to be equal to the current symbol duplicated as many times as the div (accumulator number / current number),
+  # * Update the acc number to be equal to the rem (accumulator number, current number)
+  # for example if the accumulator number is 3521 and the current number/symbol is M/1000 and the accumulator is %{n: 3521, result: ""}
+  # then the accumulator result will be "M" duplicated 3521 div 1000 = 3 times (i.e MMM)
+  # and then accumulator n will be 3521 mod 1000 = 521.
+  defp reducer({curr_num, curr_symb}, %{n: acc_num, result: _} = acc) do
+    if acc_num >= curr_num,
+      do:
+        acc
+        |> Map.update!(:result, &(&1 <> String.duplicate(curr_symb, div(acc_num, curr_num))))
+        |> Map.update!(:n, &rem(&1, curr_num)),
+      else: acc
+  end
 
   @doc """
   Convert the number to a roman number.
+
+  It will reduce on the defined symbols using the reducer and return the "result" key of the
+  accumulator. It starts with an empty result and an initial number equal to the passed num
   """
   @spec numerals(pos_integer) :: String.t()
-  def numerals(number) when is_integer(number) and number < 10 do
-    to_roman_digit({number, 1})
-  end
-
-  def numerals(number) when is_integer(number) and number < 100 do
-    to_roman_digit({div(number, 10), 10}) <> numerals(rem(number, 10))
-  end
-
-  def numerals(number) when is_integer(number) and number < 1000 do
-    to_roman_digit({div(number, 100), 100}) <> numerals(rem(number, 100))
-  end
-
   def numerals(number) when is_integer(number) and number <= 3999 do
-    to_roman_digit({div(number, 1000), 1000}) <> numerals(rem(number, 1000))
-  end
-
-  # Convert a digit to roman using different symbols depending on its base
-  # i.e if the base is 1 it will use I V and X
-  # if the base is 10 it will use X L and D
-  defp to_roman_digit({digit, base}) do
-    cond do
-      digit in [1, 2, 3] -> @symbols[base] |> String.duplicate(digit)
-      digit in [4] -> @symbols[base] <> @symbols[base * 5]
-      digit in [5] -> @symbols[base * 5]
-      digit in [6, 7, 8] -> @symbols[base * 5] <> (@symbols[base] |> String.duplicate(digit - 5))
-      digit in [9] -> @symbols[base] <> @symbols[base * 10]
-      digit == 0 -> ""
-    end
+    @symbols
+    |> Enum.reduce(
+      %{
+        n: number,
+        result: ""
+      },
+      &reducer/2
+    )
+    |> Map.get(:result)
   end
 end
