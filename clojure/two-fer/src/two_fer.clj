939a65c450aecb43ae578b4d38b19d472cd2c4d1
Serafeim Papastefanos <spapas@gmail.com>
2019-08-08 13:59:06 +0300

    small change in twofer clojure

--- clojure/two-fer/src/two_fer.clj
+++ clojure/two-fer/src/two_fer.clj
@@ -1,5 +1,5 @@
 (ns two-fer)
 
-(defn two-fer 
-  ([name] (str "One for " name ", one for me."))
-  ([] (two-fer "you")))
+(defn two-fer
+  ([] (two-fer "you"))
+  ([name] (str "One for " name ", one for me.")))
