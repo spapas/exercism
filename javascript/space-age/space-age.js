4a51ea7d04e0f91ee665e6cbf3b1926f47431b56
Serafeim Papastefanos <spapas@gmail.com>
2019-08-08 22:19:48 +0300

    small fix

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,15 +1,15 @@
 const EARTH_ORBIT_IN_S = 31557600;
-const PLANET_ORBITS_IN_S = {
-  'earth': EARTH_ORBIT_IN_S,
-  'mercury': EARTH_ORBIT_IN_S * 0.2408467,
-  'venus': EARTH_ORBIT_IN_S * 0.61519726,
-  'mars': EARTH_ORBIT_IN_S * 1.8808158,
-  'jupiter': EARTH_ORBIT_IN_S * 11.862615,
-  'saturn': EARTH_ORBIT_IN_S * 29.447498,
-  'uranus': EARTH_ORBIT_IN_S * 84.016846,
-  'neptune': EARTH_ORBIT_IN_S * 164.79132
+const PLANET_ORBITS_RATIOS = {
+  'earth': 1,
+  'mercury': 0.2408467,
+  'venus': 0.61519726,
+  'mars': 1.8808158,
+  'jupiter': 11.862615,
+  'saturn': 29.447498,
+  'uranus': 84.016846,
+  'neptune': 164.79132
 }
 
 export const age = (planet, seconds) => {
-  return +(seconds / PLANET_ORBITS_IN_S[planet]).toFixed(2);
+  return +(seconds / (EARTH_ORBIT_IN_S * PLANET_ORBITS_RATIOS[planet])).toFixed(2);
 };
