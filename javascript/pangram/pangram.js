ab0f0c0e51ae368d8765c7206998da386f838a26
Serafeim Papastefanos <spapas@gmail.com>
2019-08-09 17:14:02 +0300

    Fix js pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -2,6 +2,6 @@
 const ALL_LETTERS = [..."abcdefghijklmnopqrstuvwxyz"]
 
 export const isPangram = (phrase) => {
-  let phrase_array = [...phrase].map(x=>x.toLowerCase())
+  let phrase_array = [...phrase.toLowerCase()]
   return ALL_LETTERS.every(x => phrase_array.includes(x));
 };
