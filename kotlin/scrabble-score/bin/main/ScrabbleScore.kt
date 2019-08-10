423e5402c35d5c054ecee5b6c256bbb15d2b99bf
Serafeim Papastefanos <spapas@gmail.com>
2019-08-10 23:52:56 +0300

    imrpove kotlin scrabble

--- kotlin/scrabble-score/bin/main/ScrabbleScore.kt
+++ kotlin/scrabble-score/bin/main/ScrabbleScore.kt
@@ -1,28 +1,17 @@
 class ScrabbleScore {
    companion object {
-      val scores = mapOf(
-         'D' to 2, 
-         'G' to 2,
-         'B' to 3,
-         'C' to 3,
-         'M' to 3,
-         'P' to 3,
-         'F' to 4,
-         'H' to 4,
-         'V' to 4,
-         'W' to 4,
-         'Y' to 4,
-         'K' to 5,
-         'J' to 8,
-         'X' to 8,
-         'Q' to 10,
-         'Z' to 10
-      ) 
-
       fun scoreWord(word: String): Int {
-         return word.map {
-            scores.getOrDefault(it.toUpperCase(), 1)
-         }.sum()
+         return word.sumBy {
+            when(it.toUpperCase()) {
+               'D', 'G' -> 2
+               'B', 'C', 'M', 'P' -> 3
+               'F', 'H', 'V', 'W', 'Y' -> 4
+               'K' -> 5
+               'J', 'X' -> 8
+               'Q', 'Z' -> 10
+               else -> 1
+            }
+         }
       }
    }
 }
