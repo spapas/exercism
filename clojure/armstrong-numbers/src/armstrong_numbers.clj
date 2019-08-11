c27632ff55493abf1ec1e5fc827c79709fa66fdb
Serafeim Papastefanos <spapas@gmail.com>
2019-08-11 19:19:23 +0300

    improve kotlin scrabble

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,8 +1,8 @@
 (ns armstrong-numbers)
 
 (defn armstrong? [num]
-  (let [digits (str num) 
-    exp (count (str num))
-    sum #(reduce + %1)
-    power-mapper #(-> %1 (Character/digit 10) biginteger (.pow exp))]
-  (= (sum (map power-mapper digits)) num)))
+  (let [digits (str num)
+        exp (count (str num))
+        sum #(reduce + %1)
+        power-mapper #(-> %1 (Character/digit 10) biginteger (.pow exp))]
+    (= (sum (map power-mapper digits)) num)))
--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,17 +1,13 @@
-class ScrabbleScore {
-   companion object {
-      fun scoreWord(word: String): Int {
-         return word.sumBy {
-            when(it.toUpperCase()) {
-               'D', 'G' -> 2
-               'B', 'C', 'M', 'P' -> 3
-               'F', 'H', 'V', 'W', 'Y' -> 4
-               'K' -> 5
-               'J', 'X' -> 8
-               'Q', 'Z' -> 10
-               else -> 1
-            }
-         }
+object ScrabbleScore {
+   fun scoreWord(word: String): Int = word.sumBy {
+      when(it.toUpperCase()) {
+         'D', 'G' -> 2
+         'B', 'C', 'M', 'P' -> 3
+         'F', 'H', 'V', 'W', 'Y' -> 4
+         'K' -> 5
+         'J', 'X' -> 8
+         'Q', 'Z' -> 10
+         else -> 1        
       }
    }
 }
