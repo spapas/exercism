b03979ab791b85688c082b66f6ed6fabdcf3e542
Serafeim Papastefanos <spapas@gmail.com>
2019-08-11 19:19:05 +0300

    improve kotlin isogram

--- kotlin/isogram/bin/main/Isogram.kt
+++ kotlin/isogram/bin/main/Isogram.kt
@@ -1,8 +1,6 @@
-class Isogram {
-    companion object {
-        fun isIsogram(s: String): Boolean {
-            val filtered = s.toLowerCase().filter{it!=' ' && it!='-'}
-            return filtered.count() == filtered.toSet().count()
-        }
+object Isogram {
+    fun isIsogram(s: String): Boolean {
+        val filtered = s.toLowerCase().filter{it!=' ' && it!='-'}
+        return filtered.count() == filtered.toSet().count()
     }
 }
