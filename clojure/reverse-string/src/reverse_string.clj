9f264007c997ecf6d9a1c1ae5516c0937cd72e35
Serafeim Papastefanos <spapas@gmail.com>
2019-08-11 19:44:41 +0300

    improve clojure reverse str

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,11 +1,4 @@
 (ns reverse-string)
 
-
-(defn- reverse-string-tc [s acc]
-  (if (= (count s) 0)
-    acc
-    (recur (rest s) (cons (first s) acc))))
-
 (defn reverse-string [s]
-  (clojure.string/join "" 
-  (reverse-string-tc s [])))
+  (clojure.string/join "" (reduce conj '() s)))
