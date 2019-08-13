b4165145d03777a10931dd13bf4dbb86e381fff9
Serafeim Papastefanos <spapas@gmail.com>
2019-08-13 16:43:13 +0300

    Fix clojure bob

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,11 +1,17 @@
 (ns bob
   (:require [clojure.string :as str]))
 
+(defn- is-question? [s]
+  (= (last s) \?))
+
+(defn- is-upcase? [s]
+  (and (not= s (str/lower-case s)) (= s (str/upper-case s))))
+
 (defn response-for [s]
   (cond
     (= (str/trim s) "") "Fine. Be that way!"
-    (and (= (last s) \?) (not= s (str/lower-case s)) (= s (str/upper-case s))) "Calm down, I know what I'm doing!"
-    (and (not= s (str/lower-case s)) (= s (str/upper-case s))) "Whoa, chill out!"
-    (= (last s)  \?) "Sure."
+    (and (is-question? s) (is-upcase? s)) "Calm down, I know what I'm doing!"
+    (is-upcase? s) "Whoa, chill out!"
+    (is-question? s) "Sure."
     :else "Whatever."))
 
