bbb6a11f564470179adaca7b54ef41f3b9406531
Serafeim Papastefanos <spapas@gmail.com>
2019-08-15 17:29:12 +0300

    improve kotlin squares

--- kotlin/difference-of-squares/src/main/kotlin/Squares.kt
+++ kotlin/difference-of-squares/src/main/kotlin/Squares.kt
@@ -1,16 +1,12 @@
 class Squares(val number: Int) {
-    fun squareOfSum(): Int {
-        val s = (1..number).sum()
-        return s*s
+    fun squareOfSum(): Int = (1..number).sum().let {
+        it * it
     }
 
-    fun sumOfSquares(): Int {
-        return (1..number).sumBy {
-            it * it
-        }
+    fun sumOfSquares(): Int = (1..number).sumBy {
+        it * it
     }
-
-    fun difference(): Int {
-        return squareOfSum() - sumOfSquares()
-    }
+    
+    fun difference(): Int = squareOfSum() - sumOfSquares()
+    
 }
