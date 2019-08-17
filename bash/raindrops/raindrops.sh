de730ec8d19059a7d5e621055b8d8853cb0c2b3a
Serafeim Papastefanos <spapas@gmail.com>
2019-08-18 00:02:11 +0300

    add bash raindrops

---
+++ bash/raindrops/raindrops.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+
+main () {
+
+    if (( $1 % 3 == 0 ))
+    then
+        echo -n "Pling"
+    fi
+
+    if (( $1 % 5 == 0 ))
+    then
+        echo -n "Plang"
+    fi
+
+    if (( $1 % 7 == 0 ))
+    then
+        echo -n "Plong"
+    fi
+
+    if (( $1 % 3 != 0 && $1 % 5 != 0 && $1 % 7 != 0 ))
+    then
+        echo $1
+    fi
+}
+
+main "$@"
