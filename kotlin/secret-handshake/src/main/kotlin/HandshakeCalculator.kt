d3be49f2fbb207a5cd75b789578969c707df2dc8
Serafeim Papastefanos <spapas@gmail.com>
2019-08-18 16:35:13 +0300

    A different way of kotlin handshake

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -2,16 +2,15 @@
 
 object HandshakeCalculator {
     fun calculateHandshake(n: Int): List<Signal> = listOf(
-        Signal.WINK,
-        Signal.DOUBLE_BLINK,
-        Signal.CLOSE_YOUR_EYES,
-        Signal.JUMP,
-        null
-    ).foldIndexed(listOf<Signal>()) { idx, acc, it ->
-        if(n and ((Math.pow(2.0, 1.0*idx)).toInt()) !=0) {
-            if(it!=null) acc + it else acc.asReversed()
-        } else {
-            acc
-        }
+        Pair(n and 1 != 0, Signal.WINK),
+        Pair(n and 2 != 0, Signal.DOUBLE_BLINK),
+        Pair(n and 4 != 0, Signal.CLOSE_YOUR_EYES),
+        Pair(n and 8 != 0, Signal.JUMP)
+    ).filter {
+        it.first == true
+    }.map {
+        it.second
+    }.let {
+        if (n and 16 == 0) it else it.asReversed()
     }
 }
