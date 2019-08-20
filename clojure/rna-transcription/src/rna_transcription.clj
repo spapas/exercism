fc97c9d5eb93e2736b700d6ecfbfa72d8dd9b3a9
Serafeim Papastefanos <spapas@gmail.com>
2019-08-20 13:13:32 +0300

    improve clojure rna

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,14 +1,10 @@
 (ns rna-transcription
   (:require [clojure.string :as str]))
 
+(def dna->rna {\G \C \C \G \T \A \A \U})
 
 (defn to-rna [dna]
-  (str/join
-   ""
-   (map #(case %1
-           \G \C
-           \C \G
-           \T \A
-           \A \U
-           (throw (AssertionError. "Wrong input.")))
-        dna)))
+  {:pre  [(every? #(contains? (set (keys dna->rna)) %1) dna)]}
+  (->> dna
+       (map dna->rna)
+       (str/join "")))
