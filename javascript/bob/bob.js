05d249bab9907515680fb4a47d3b2fe045fa0f5d
Serafeim Papastefanos <spapas@gmail.com>
2019-08-22 17:33:53 +0300

    improve bobjs

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,20 +1,19 @@
 
 export const hey = (message) => {
-  let isQuestion = s => s.slice(-1) == "?"
-  let isCaps = s => s.toUpperCase() == s && s.toLowerCase() != s
-  let isEmpty = s => s == ""
-  
+  let isQuestion = s => s.slice(-1) === "?"
+  let isCaps = s => s.toUpperCase() === s && s.toLowerCase() != s
+  let isEmpty = s => s === ""
+
   message = message.trim()
-  if(isEmpty(message)) {
+  if (isEmpty(message)) {
     return "Fine. Be that way!"
-  } else if(isCaps(message) && isQuestion(message)) {
+  } else if (isCaps(message) && isQuestion(message)) {
     return "Calm down, I know what I'm doing!"
-  } else if(isCaps(message)) {
+  } else if (isCaps(message)) {
     return "Whoa, chill out!"
-  } else if(isQuestion(message)) {
+  } else if (isQuestion(message)) {
     return "Sure."
   } else {
     return "Whatever."
   }
-  
 };
