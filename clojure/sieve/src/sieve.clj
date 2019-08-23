b351c5d5c5db49c7ac16e7f39703993f5b591059
Serafeim Papastefanos <spapas@gmail.com>
2019-08-23 07:50:38 +0300

    fix clojrue sieve

--- clojure/sieve/src/sieve.clj
+++ clojure/sieve/src/sieve.clj
@@ -1,16 +1,18 @@
 (ns sieve)
 
+(defn not-in [col el] (not (some #(= el %1) col)))
+
 (defn- do-sieve [numbers primes]
   (cond
     (empty? numbers) primes
     :else (let [current-number (first numbers)
                 new-primes (conj primes current-number)
                 last-number (last numbers)
-                to-sieve (into [] (range current-number last-number current-number))
-                sieved (map #(not (contains? to-sieve %1)) numbers)]
-            (do-sieve sieved new-primes))))
+                to-sieve (range current-number (+ 1 last-number) current-number)
+                sieved (filter #(not-in to-sieve %1) numbers)]
+            (recur sieved new-primes))))
 
 
 (defn sieve [number]
-  (do-sieve (range 2 number) []))
+  (do-sieve (range 2 (+ 1 number)) []))
 
