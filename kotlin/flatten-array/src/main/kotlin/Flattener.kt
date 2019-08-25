af421ef6968e7b51601a98822cba39138b3457f4
Serafeim Papastefanos <spapas@gmail.com>
2019-08-25 09:06:35 +0300

    Add kotlin flatten array

---
+++ kotlin/flatten-array/src/main/kotlin/Flattener.kt
@@ -0,0 +1,16 @@
+ object Flattener {
+    fun flatten(list: List<Any?>): List<Any?> = list.fold(listOf()) {
+        acc, el -> reducer(acc, el)
+    }
+
+    fun reducer(acc: List<Any?>, el: Any?): List<Any?> = if(el == null) {
+        acc
+    } else if (el is List<*>) {
+        el.fold(acc) {
+            internalAcc, internalEl -> reducer(internalAcc, internalEl)
+        }
+    } else {
+        acc + el
+    }
+
+ }
