9c803b0ad6b6b2bc8f6fdb081ef1cea942ea3b83
Serafeim Papastefanos <spapas@gmail.com>
2019-08-28 16:46:31 +0300

    improve bash acronym

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -3,15 +3,14 @@
 
 main () {
     idx=0
-    cnt=0
-    #echo ${1^}
     letter=${1:0:1}
-    letters=${letter^}
-    
+    if [[ $letter =~ [[:alpha:]] ]]; then
+        letters=${letter^}
+    fi
+
     until (( $idx >= ${#1} )); do
-        #echo ${1:$idx+1:1}
-        if [[ ${1:$idx:1} == " " || ${1:$idx:1} == "-" || ${1:$idx:1} == "_" ]]; then
-            letter=${1:$idx+1:1}
+        if [[ ${1:idx:1} == [" "_-] ]]; then
+            letter=${1:idx+1:1}
             if [[ $letter =~ [[:alpha:]] ]]; then
                 letters+=${letter^}
                 ((idx+=1))
