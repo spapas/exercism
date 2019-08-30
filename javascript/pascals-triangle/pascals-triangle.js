f19d2551144780a65944462419df74a9266baeeb
Serafeim Papastefanos <spapas@gmail.com>
2019-08-30 11:23:45 +0300

    improve pascal js

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,35 +1,33 @@
-
 export class Triangle {
   constructor(number) {
     this.number = number;
   }
 
   get lastRow() {
-    return this.createRows()[this.number - 1]
+    return this.rows[this.number - 1]
   }
 
   get rows() {
+    if (this._rows) {
+      return this._rows
+    }
     return this.createRows()
   }
 
   createRows() {
-    // Don't calculate this again
-    if (this._rows) {
-      return this._rows
-    }
-    // This is the 1st row
     this._rows = [[1]]
-    // For each row
     for (var i = 1; i < this.number; i++) {
-      // Push the rightmost number
-      this._rows.push([1])
-      // Calculate the intermediate numbers by taking the sum of the corresponding vals of the previous row
-      for (var j = 1; j < i; j++) {
-        this._rows[i][j] = this._rows[i - 1][j - 1] + this._rows[i - 1][j]
-      }
-      // Push the leftmost number
-      this._rows[j].push(1)
+      this.createRow(i)
     }
     return this._rows
+  }
+
+  createRow(index) {
+    this._rows.push([1])
+    // Calculate the intermediate numbers by taking the sum of the corresponding vals of the previous row
+    for (var j = 1; j < index; j++) {
+      this._rows[index][j] = this._rows[index - 1][j - 1] + this._rows[index - 1][j]
+    }
+    this._rows[j].push(1)
   }
 }
