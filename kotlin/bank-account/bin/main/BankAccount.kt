009562a6905629e4aa00a1c980feecdc827f832d
Serafeim Papastefanos <spapas@gmail.com>
2019-09-03 22:18:42 +0300

    ok

--- kotlin/bank-account/bin/main/BankAccount.kt
+++ kotlin/bank-account/bin/main/BankAccount.kt
@@ -1,20 +1,24 @@
 class BankAccount() {
-    var isOpen: Boolean = true
-    var balanceAmount: Int = 0
+    private var isOpen: Boolean = true
+    private var balanceAmount: Int = 0
 
     val balance: Int
     get() {
-        if(!isOpen) throw IllegalStateException()
+        mustBeOpen()
         return balanceAmount
     }
 
     @Synchronized fun adjustBalance (amt: Int) {
-        if(!isOpen) throw IllegalStateException()
+        mustBeOpen()
         balanceAmount += amt
     }
 
     fun close() {
-        if(!isOpen) throw IllegalStateException()
+        mustBeOpen()
         isOpen = false
+    }
+
+    private fun mustBeOpen() {
+        if(!isOpen) throw IllegalStateException()
     }
 }
