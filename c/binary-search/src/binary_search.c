60db6db4a51ec0f28ace83451d59b88d035731e4
Serafeim Papastefanos <spapas@gmail.com>
2019-09-06 23:17:12 +0300

    Add bin search non recursive

--- c/binary-search/src/binary_search.c
+++ c/binary-search/src/binary_search.c
@@ -3,6 +3,32 @@
 
 int *binary_search(int value, const int *arr, size_t length)
 {
+    size_t len = length;
+    int *start = (int *)arr;
+    while (len > 0)
+    {
+        size_t idx = len / 2;
+        int middleValue = *(start + idx);
+        if (middleValue == value)
+        {
+            return start + idx;
+        }
+        else if (middleValue > value)
+        {
+            len /= 2;
+        }
+        else
+        {
+            start = (int *)start + idx + 1;
+            len /= 2;
+        }
+    }
+    return NULL;
+}
+
+/*
+int *binary_search_recursive(int value, const int *arr, size_t length)
+{
     if (length == 0)
         return NULL;
     int idx = length / 2;
@@ -20,4 +46,4 @@ int *binary_search(int value, const int *arr, size_t length)
     {
         return binary_search(value, arr + idx + 1, length / 2);
     }
-}
+}*/
