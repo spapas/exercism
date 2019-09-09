5e98a039807e390d204e29351a5a4d6b2274b5b4
Serafeim Papastefanos <spapas@gmail.com>
2019-09-09 21:32:13 +0300

    ok

--- python/hangman/hangman.py
+++ python/hangman/hangman.py
@@ -9,15 +9,15 @@ class Hangman(object):
         self.remaining_guesses = 9
         self.status = STATUS_ONGOING
         self.word = word
-        self.letters = set(l for l in word)
+        self.letters_left = set(l for l in word)
 
     def guess(self, char):
         if self.status != STATUS_ONGOING:
             raise ValueError("Game over!")
 
-        if char in self.letters:
-            self.letters.remove(char)
-            if len(self.letters) == 0:
+        if char in self.letters_left:
+            self.letters_left.remove(char)
+            if not self.letters_left:
                 self.status = STATUS_WIN
         else:
             self.remaining_guesses-=1
@@ -25,7 +25,7 @@ class Hangman(object):
                 self.status = STATUS_LOSE
 
     def get_masked_word(self):
-        return ''.join('_' if c in self.letters else c for c in self.word)
+        return ''.join('_' if c in self.letters_left else c for c in self.word)
 
     def get_status(self):
         return self.status
