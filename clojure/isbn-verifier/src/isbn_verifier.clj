69938004e38675557b55192f87f5f5f696f38285
Serafeim Papastefanos <spapas@gmail.com>
2019-09-12 15:00:42 +0300

    add clojure isbn

---
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -0,0 +1,25 @@
+(ns isbn-verifier)
+
+(defn- to-num-array [isbn]
+  "convert to a number array; X will be converted to 10"
+  (map #(if (= % \X) 10 (Integer/parseInt (str %))) isbn))
+
+(defn- calculate-checksum [numbers]
+  "calc the checksum; multiply each number by 10 - idx (so the first number) will by multipled by 10 etc
+  and get the sum"
+  (->> numbers
+       (map-indexed (fn [idx el] (* el (- 10 idx))))
+       (reduce +)))
+
+(defn isbn? [isbn]
+  "first make sure that the isbn is valid: contains 9 digits and a digit or X; also remove
+  any dashes but don't allow anything else. Then pass the normalized isbn to to-num-array
+  to get an array of numbers and to calculate-checksum to get the actualy checksum. Finally
+  make sure that this checksum mod 11 == 0"
+  (let [normalized-isbn (re-find #"^\d{9}[X\d]$" (clojure.string/replace isbn "-" ""))]
+    (if normalized-isbn (->
+                         normalized-isbn
+                         to-num-array
+                         calculate-checksum
+                         (mod 11)
+                         (= 0)) false)))
