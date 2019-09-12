5f9bb055e0d87545c5e59dcfb87f3e77d13900c2
Serafeim Papastefanos <spapas@gmail.com>
2019-09-12 18:39:32 +0300

    Change bash pangram

--- bash/pangram/pangram.sh
+++ bash/pangram/pangram.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-main () {
+main2 () {
     # Remove non-alpha characters
     # Covert to lowercase
     # Put each character in a separate line
@@ -15,6 +15,37 @@ main () {
     else
         echo "false"
     fi;
+}
+
+
+ALL_CHARS="abcdefghijklmnopqrstuvwxyz"
+
+main () {
+    input="$1"
+    is_pangram=1
+    for (( i = 0; i < ${#ALL_CHARS}; i++ )); do
+        char_to_check=${ALL_CHARS:$i:1}
+        char_found=0
+        for (( j = 0; j < ${#input}; j++ )); do
+            char=${input:$j:1}
+            char=${char,}
+            if [[ $char == $char_to_check ]]; then
+                char_found=1
+                break
+            fi;
+        done
+        if [[ $char_found -eq 0 ]]; then
+            is_pangram=0
+            break
+        fi;
+    done
+
+    if [[ $is_pangram -eq 1 ]]; then
+        echo "true"
+    else
+        echo "false"
+    fi;
+
 }
 
 main "$@"
