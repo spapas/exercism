c64df3b5f482d35647271177223788b415bdbcca
Serafeim Papastefanos <spapas@gmail.com>
2019-09-12 23:13:26 +0300

    fix bash pangram

--- bash/pangram/pangram.sh
+++ bash/pangram/pangram.sh
@@ -17,30 +17,17 @@ main2 () {
     fi;
 }
 
-
-ALL_CHARS="abcdefghijklmnopqrstuvwxyz"
-
 main () {
-    input="$1"
+    input=${1,,}
     is_pangram=1
-    for (( i = 0; i < ${#ALL_CHARS}; i++ )); do
-        char_to_check=${ALL_CHARS:$i:1}
-        char_found=0
-        for (( j = 0; j < ${#input}; j++ )); do
-            char=${input:$j:1}
-            char=${char,}
-            if [[ $char == $char_to_check ]]; then
-                char_found=1
-                break
-            fi;
-        done
-        if [[ $char_found -eq 0 ]]; then
+    for char_to_check in {a..z}; do
+        if [[ ! $input =~ $char_to_check ]]; then
             is_pangram=0
             break
-        fi;
+        fi
     done
 
-    if [[ $is_pangram -eq 1 ]]; then
+    if (( $is_pangram == 1 )); then
         echo "true"
     else
         echo "false"
