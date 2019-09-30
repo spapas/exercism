298aca4f0a3f72691c51f59546f4620e8ddbaa9f
Serafeim Papastefanos <spapas@gmail.com>
2019-09-30 08:08:01 +0300

    Add handshake calc java

---
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
+
+class HandshakeCalculator {
+
+    List<Signal> calculateHandshake(int number) {
+        List<Signal> r = new ArrayList<Signal>();
+        if( (number & 1) !=0) r.add(Signal.WINK);
+        if( (number & 2) !=0) r.add(Signal.DOUBLE_BLINK);
+        if( (number & 4) !=0) r.add(Signal.CLOSE_YOUR_EYES);
+        if( (number & 8) !=0) r.add(Signal.JUMP);
+        if( (number & 16) !=0) Collections.reverse(r);
+        return r;
+        
+    }
+
+}
