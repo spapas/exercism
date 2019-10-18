8b9ce186bbfc56f5f94801120758ac568ad32483
Serafeim Papastefanos <spapas@gmail.com>
2019-10-18 14:57:58 +0300

    improve py 12days

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,38 +1,39 @@
-NUMERALS = {
-    1: 'first',
-    2: 'second',
-    3: 'third',
-    4: 'fourth',
-    5: 'fifth',
-    6: 'sixth',
-    7: 'seventh',
-    8: 'eighth',
-    9: 'ninth',
-    10: 'tenth',
-    11: 'eleventh',
-    12: 'twelfth',
-}
+NUMERALS = [
+    '',
+    'first',
+    'second',
+    'third',
+    'fourth',
+    'fifth',
+    'sixth',
+    'seventh',
+    'eighth',
+    'ninth',
+    'tenth',
+    'eleventh',
+    'twelfth',
+]
 
-THINGS = {
-    1: 'a Partridge in a Pear Tree',
-    2: 'two Turtle Doves',
-    3: 'three French Hens',
-    4: 'four Calling Birds',
-    5: 'five Gold Rings',
-    6: 'six Geese-a-Laying',
-    7: 'seven Swans-a-Swimming',
-    8: 'eight Maids-a-Milking',
-    9: 'nine Ladies Dancing',
-    10: 'ten Lords-a-Leaping',
-    11: 'eleven Pipers Piping',
-    12: 'twelve Drummers Drumming',
-}
-
+THINGS = [
+    '',
+    'a Partridge in a Pear Tree',
+    'two Turtle Doves',
+    'three French Hens',
+    'four Calling Birds',
+    'five Gold Rings',
+    'six Geese-a-Laying',
+    'seven Swans-a-Swimming',
+    'eight Maids-a-Milking',
+    'nine Ladies Dancing',
+    'ten Lords-a-Leaping',
+    'eleven Pipers Piping',
+    'twelve Drummers Drumming',
+]
 
 def create_things(verse):
     if verse == 1:
         return THINGS[verse]
-    return f"{', '.join(THINGS[n] for n in range(verse, 1, -1))}, and {THINGS[1]}"
+    return f"{', '.join(THINGS[verse:1:-1])}, and {THINGS[1]}"
 
 
 def recite_verse(verse):
