d899dea8487dcdc07ea0bdb2184cd131317c5421
Serafeim Papastefanos <spapas@gmail.com>
2019-11-06 09:18:54 +0200

    ok

--- c/square-root/src/square_root.c
+++ c/square-root/src/square_root.c
@@ -1,11 +1,15 @@
 #include "square_root.h"
 
 unsigned int squareRoot(unsigned int n) {
-    unsigned int i;
-    for(i=0;i<=n;i++) {
-        if (i*i == n) {
+    for(unsigned int i=0;i<=n;i++) {
+        unsigned int sq = i*i;
+        if (sq == n) {
             return i;
         }
+        if(sq > n) {
+            return 0;
+        }
+        
     }
     return 0;
 }
