1bd9be5a718dd0f7d10d40998ad810545982a91e
Serafeim Papastefanos <spapas@gmail.com>
2019-11-25 21:37:47 +0200

    smfix

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -1,13 +1,13 @@
 
 export const secretHandshake = (n) => {
   let r = []
-  if(n!=Number(n)) {
+  if (isNaN(n)) {
     throw Error("Handshake must be a number")
   }
-  if((n & 1) !=0) r.push('wink')
-  if((n & 2) !=0) r.push('double blink')
-  if((n & 4) !=0) r.push('close your eyes')
-  if((n & 8) !=0) r.push('jump')        
-  if((n & 16) !=0) r.reverse()  
+  if ((n & 1) != 0) r.push('wink')
+  if ((n & 2) != 0) r.push('double blink')
+  if ((n & 4) != 0) r.push('close your eyes')
+  if ((n & 8) != 0) r.push('jump')
+  if ((n & 16) != 0) r.reverse()
   return r
 };
