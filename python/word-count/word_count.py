37cf2c646b31a1a5b99fbfc0eb017baaad73ee28
Serafeim Papastefanos <spapas@gmail.com>
2019-12-05 21:27:28 +0200

    ok

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,15 +1,20 @@
 import re
+from collections import Counter
+
+IGNORED = "[^a-z0-9' ]"
+SEPARATOR = " "
 
 def count_words(sentence):
     lower_sentence = sentence.lower()
-    words = re.sub("[^a-z0-9' ]"," ",lower_sentence).split(" ")
+    words = re.sub(IGNORED, SEPARATOR, lower_sentence).split(SEPARATOR)
 
-    result = {}
+    result = Counter()
+
     for word in words:
         if not word:
             continue
         if word[0] == "'":
             word = word[1:-1]
-        val = result.get(word, 0)
-        result[word] = val+1
+        result.update([word])
+
     return result
