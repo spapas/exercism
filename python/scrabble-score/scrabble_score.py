78b53cf5dfd3c78f59d35c5162b8261c2eeb5aa2
Serafeim Papastefanos <spapas@gmail.com>
2019-12-31 17:16:35 +0200

    ok

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,6 +1,8 @@
 from enum import IntEnum
 
+
 class LetterValues(IntEnum):
+    A = E = I = O = U = L = N = R = S = T = 1
     D = G = 2
     B = C = M = P = 3
     F = H = V= W = Y = 4
@@ -8,11 +10,6 @@ class LetterValues(IntEnum):
     J = X = 8
     Q = Z = 10
 
-def score_letter(l):
-    try:
-        return LetterValues[l]
-    except:
-        return 1
 
 def score(word):
-    return sum(score_letter(z) for z in word.upper())
+    return sum(LetterValues[z] for z in word.upper())
