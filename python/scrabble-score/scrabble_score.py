9dd2c441bb8535d9aca5a559333727529b84261a
Serafeim Papastefanos <spapas@gmail.com>
2019-12-31 15:19:28 +0200

    use enums for scrabble

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,16 +1,18 @@
-from collections import defaultdict
+from enum import IntEnum
 
-LETTER_VALUES = defaultdict(lambda: 1)
-LETTER_VALUES['D'] = LETTER_VALUES['G'] = 2
-LETTER_VALUES['B'] = LETTER_VALUES['C'] = LETTER_VALUES['M'] = LETTER_VALUES['P'] = 3
-LETTER_VALUES['F'] = LETTER_VALUES['H'] = LETTER_VALUES['V'] = LETTER_VALUES['W'] = LETTER_VALUES['Y'] = 4
-LETTER_VALUES['K'] = 5
-LETTER_VALUES['J'] = LETTER_VALUES['X'] = 8
-LETTER_VALUES['Q'] = LETTER_VALUES['Z'] = 10
-
+class LetterValues(IntEnum):
+    D = G = 2
+    B = C = M = P = 3
+    F = H = V= W = Y = 4
+    K = 5
+    J = X = 8
+    Q = Z = 10
 
 def score_letter(l):
-    return LETTER_VALUES[l]
+    try:
+        return LetterValues[l]
+    except:
+        return 1
 
 def score(word):
     return sum(score_letter(z) for z in word.upper())
