ddbff20a1b19908238ed5e8805b209b01181b7ef
Serafeim Papastefanos <spapas@gmail.com>
2020-02-07 13:53:29 +0200

    fix luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -8,15 +8,14 @@ class Luhn:
     def valid(self):
         if re.search('[^0-9 ]', self.card_num):
             return False
+        
+        clean_card_num = self.card_num.replace(' ','').replace('-','')
+        digits = list(reversed([int(d) for d in clean_card_num]))
 
-        digits = [int(d) for d in self.card_num.replace(' ','').replace('-','')]
-
-        if len(digits) == 1 and digits[0] == 0:
+        if not digits or len(digits) == 1 and digits[0] == 0:
             return False
 
-        acc = 0
-        for idx, d in enumerate(reversed(digits)):
-            acc += (d * 2 if d < 5 else d * 2 - 9) if idx % 2 else d
-
+        acc = sum((d * 2 if d < 5 else d * 2 - 9) if idx % 2 else d for idx, d in enumerate(digits))
+            
         return False if acc % 10 else True
         
