d215e0193a7f2dd3796468c25218021b818f0901
Serafeim Papastefanos <spapas@gmail.com>
2020-03-12 08:26:30 +0200

    ok

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -3,24 +3,23 @@ class Clock:
         hour_norm = hour % 24
         minutes_norm = minute % 60
         self.minutes = hour_norm * 60 + minute
-        
-    def __repr__(self):
         if self.minutes < 0 :
             self.minutes = 24*60 + self.minutes
-        h = self.minutes // 60
+        
+    def __repr__(self):      
+        h = (self.minutes // 60) % 24
         m = self.minutes % 60
-        h = h % 24
         return f'{h:02}:{m:02}'
 
     def __eq__(self, other):
         return str(self) == str(other)
 
     def __add__(self, minutes):
-        self.minutes += minutes
-        return self
+        return Clock(0, self.minutes + minutes)
+        
 
     def __sub__(self, minutes):
-        self.minutes -= minutes
+        return Clock(0, self.minutes - minutes)
         return self
 
     
