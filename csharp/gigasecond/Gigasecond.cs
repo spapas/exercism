b6f52719310b29a0c2f3287fc6c22c5cec60ca49
Serafeim Papastefanos <spapas@gmail.com>
2020-10-10 09:17:47 +0300

    Fix gigasecond cs

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -2,9 +2,7 @@ using System;
 
 public static class Gigasecond
 {
-    static readonly int GS = 1000000000;
-    public static DateTime Add(DateTime moment)
-    {
-        return moment.AddSeconds(GS);
-    }
+    private const int GS = 1_000_000_000;
+    public static DateTime Add(DateTime moment) => moment.AddSeconds(GS);
+    
 }
