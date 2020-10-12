c1eef3b53b2e5c89cfb38bde97250df25b5d1d88
Serafeim Papastefanos <spapas@gmail.com>
2020-10-12 12:29:57 +0300

    Add hamming cs

---
+++ csharp/hamming/Hamming.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        if (firstStrand.Length == 0 && secondStrand.Length == 0) return 0;
+        if (firstStrand.Length != secondStrand.Length) throw new ArgumentException();
+        if (firstStrand.Length == 0 || secondStrand.Length == 0) throw new ArgumentException();
+        return firstStrand.Zip(secondStrand, (first, second) => (first == second) ? 0 : 1).Sum();
+
+    }
+}
