12f466818bf7abf87b83b55614f715d5b87bb9b7
Serafeim Papastefanos <spapas@gmail.com>
2020-10-14 09:22:42 +0300

    Improve hamming

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -5,9 +5,9 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        if (firstStrand.Length == 0 && secondStrand.Length == 0) return 0;
+        //if (firstStrand.Length == 0 && secondStrand.Length == 0) return 0;
         if (firstStrand.Length != secondStrand.Length) throw new ArgumentException();
-        if (firstStrand.Length == 0 || secondStrand.Length == 0) throw new ArgumentException();
+        //if (firstStrand.Length == 0 || secondStrand.Length == 0) throw new ArgumentException();
         return firstStrand.Zip(secondStrand, (first, second) => (first == second) ? 0 : 1).Sum();
 
     }
