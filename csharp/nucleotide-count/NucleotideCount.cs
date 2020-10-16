7a1a5ef4e54864c3fa6ce42863b62897df935aed
Serafeim Papastefanos <spapas@gmail.com>
2020-10-16 12:50:20 +0300

    Some fixes

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -7,10 +7,9 @@ public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        var valid = new List<char> { 'A', 'T', 'C', 'G'};
         var nucleotideCounts = new Dictionary<char, int> { {'A', 0} , {'T', 0}, {'C', 0}, {'G', 0}};
         foreach(char c in sequence.ToCharArray()) {
-          if(!valid.Contains(c)) throw new ArgumentException();
+          if(!nucleotideCounts.ContainsKey(c)) throw new ArgumentException();
           nucleotideCounts[c]++;
         }
         
