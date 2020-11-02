507ec0cd137cf3fb9e8d3cfa742d82080c90b1cf
Serafeim Papastefanos <spapas@gmail.com>
2020-11-02 12:16:28 +0200

    Add cs rot

--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Linq;
 
 public static class RotationalCipher
 {
-    public static string Rotate(string text, int shiftKey)
+    public static char rot(char c, char a, char z, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (c < a || c > z) return c;
+        char r = (char)((int)c + shiftKey % 26);
+        if (r > z)
+        {
+            int rmz = (int)r - (int)z;
+            r = (char)((int)a + rmz - 1);
+        }
+        return r;
     }
+    public static string Rotate(string text, int shiftKey) => string.Join("", text.Select(
+        c => rot(c, 'a', 'z', shiftKey)
+    ).Select(
+        c => rot(c, 'A', 'Z', shiftKey)
+    ).ToArray());
+
 }
